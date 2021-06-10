6bbf565335bdc3b6847435a787a0341ccf36bf5f
maxgamesNL <72696183+maxgamesNL@users.noreply.github.com>
2021-06-10 22:03:10 +0200

    Added the intro
    Added the intro that tells what happens. also added the MainMenu funcion where you type the commands and will be able to ask for a list of the commands and what they do.

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
+import time
+
 #This is the main file of the project.
 #On editing write documentation for easy editing.
 Running = True
+
+
+#Explains what the program is to the user.
+def Intro():
+    print("Welcome to LessonUp Bot!!!")
+    time.sleep(1.5)
+    print("This bot is able to perform")
+    time.sleep(1.5)
+    print("Lots of actions realy fast")
+    time.sleep(1.5)
+    print("It is able to answer random.")
+    time.sleep(1.5)
+    print("And custom answers")
+    time.sleep(2.5)
+    print("Start-Up Complete")
+
+#This is where the user is shown all the options of the program.
+def MainMenu():
+    print("")
+
+
+
+
+Intro()
+time.sleep(3)
+MainMenu()
 while(Running):
     
     Running = False
