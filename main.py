fb4bd92174b3cffd14f74eaec7e31f2651c2221b
maxgamesNL <72696183+maxgamesNL@users.noreply.github.com>
2021-06-11 15:18:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,24 +11,48 @@ def Intro():
     time.sleep(1.5)
     print("This bot is able to perform")
     time.sleep(1.5)
-    print("Lots of actions realy fast")
+    print("A lot of actions realy fast")
     time.sleep(1.5)
-    print("It is able to answer random.")
+    print("It is able to answer randomly.")
     time.sleep(1.5)
-    print("And custom answers")
+    print("And give custom answers")
     time.sleep(2.5)
     print("Start-Up Complete")
+    time.sleep(1)   
+    print("This is a list of all the commands.") 
+    time.sleep(1)
+    print("Type the number of your command.")
+    time.sleep(1)
+    print("And you will go to the menu of your command!")
+    print("")
 
+
+
+def CommandList():
+    print("1. Flooder(Fills the Lesson-Up with bots that give random answers.)")
+    print("")
+    print("")
+    print("")
+    print("")
+
+    print("To stop type stop.")
 #This is where the user is shown all the options of the program.
 def MainMenu():
-    print("")
+    CommandList()
+    UserInput = input("Enter the number of your command: ")
+    if(UserInput == "stop" or "Stop"):
+        Running = False
+    else:
+        UserInputNum = int(UserInput)
+    
+
+
+
 
 
 
 
 Intro()
 time.sleep(3)
-MainMenu()
 while(Running):
-    
-    Running = False
+    MainMenu()
